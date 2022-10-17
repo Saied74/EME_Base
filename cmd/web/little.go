c6cafa47690993d17f1ff4f468847583e46528f2
Saied Seghatoleslami <seghatoleslami74@gmail.com>
2022-10-17 10:40:15 -0400

    added DVRA icon to window, reversed the order of air and sink temperature on window

--- cmd/web/little.go
+++ cmd/web/little.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image/color"
+	"log"
 	"runtime/debug"
 	"strconv"
 	"time"
@@ -23,10 +24,14 @@ const (
 func (app *application) little() {
 	a := ap.New()
 	w := a.NewWindow("Temperature and Power")
-
+	dvra, err := fyne.LoadResourceFromPath("ui/static/img/dvra.jpeg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.SetIcon(dvra)
 	row1Col1 := widget.NewLabel("Amp")
-	row1Col2 := widget.NewLabel("Heatsink")
-	row1Col3 := widget.NewLabel("Air")
+	row1Col2 := widget.NewLabel("Air")
+	row1Col3 := widget.NewLabel("Heatsink")
 	row1 := container.New(layout.NewGridLayout(3), row1Col1, row1Col2, row1Col3)
 
 	td, err := app.updateSensors()
@@ -38,8 +43,8 @@ func (app *application) little() {
 	}
 
 	row2Col1 := widget.NewLabel(td.AmpPower)
-	row2Col2 := widget.NewLabel(td.SinkTemp)
-	row2Col3 := widget.NewLabel(td.AirTemp)
+	row2Col2 := widget.NewLabel(td.AirTemp)
+	row2Col3 := widget.NewLabel(td.SinkTemp)
 	row2 := container.New(layout.NewGridLayout(3), row2Col1, row2Col2, row2Col3)
 
 	grid := container.New(layout.NewVBoxLayout(), row1, row2)
@@ -55,8 +60,8 @@ func (app *application) little() {
 				app.errorLog.Output(2, trace)
 			}
 			row2Col1.SetText(td.AmpPower)
-			row2Col2.SetText(td.SinkTemp)
-			row2Col3.SetText(td.AirTemp)
+			row2Col2.SetText(td.AirTemp)
+			row2Col3.SetText(td.SinkTemp)
 
 			t, _ := strconv.ParseFloat(td.SinkTemp, 64)
 			if t > app.tempThreshold && colorCode != red {
